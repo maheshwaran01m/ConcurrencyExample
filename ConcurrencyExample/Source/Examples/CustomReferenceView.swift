1a89973fdcaad4a36cc12ac5ac33378e2e76d403
MAHESHWARAN <maheshwaran@eam360.com>
2024-03-03 16:29:19 +0530

    Source  - Reference

--- ConcurrencyExample/Source/Examples/CustomReferenceView.swift
+++ ConcurrencyExample/Source/Examples/CustomReferenceView.swift
@@ -5,4 +5,63 @@
 //  Created by MAHESHWARAN on 03/03/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct CustomReferenceView: View {
+  
+  @StateObject private var viewModel = CustomReferenceViewModel()
+  
+  var body: some View {
+    VStack {
+      Text(viewModel.text)
+    }
+  }
+}
+
+// MARK: - Preview
+
+struct CustomReferenceView_Previews: PreviewProvider {
+  static var previews: some View {
+    CustomReferenceView()
+  }
+}
+
+// MARK: - ViewModel
+
+fileprivate class CustomReferenceViewModel: ObservableObject {
+  
+  @Published var text = "Some Title"
+  
+  private var task: Task<(), Never>?
+  
+  init() {
+    fetchRecords()
+  }
+  
+  func fetchRecords() {
+    Task {
+      text = await getData()
+    }
+    
+    // not required to make weak reference
+    task = Task { [weak self] in
+      if let data = await self?.getData() {
+        self?.text = data
+      }
+    }
+    
+    // task will run in main actor
+    task = Task { @MainActor in
+      text = await getData()
+    }
+  }
+  
+  func getData() async -> String {
+    "Updated value"
+  }
+  
+  deinit {
+    task?.cancel()
+    task = nil
+  }
+}
